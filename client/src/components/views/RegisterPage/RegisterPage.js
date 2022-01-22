3c82c2dca42000cf0933b9f3a3f61fab9295d815
seoin-cho <choseoin1140@sookmyung.ac.kr>
2022-01-22 10:25:23 +0900

    #34 인증 체크 (2) 강의 마무리

--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -33,6 +33,7 @@ function RegisterPage() {
         })
         
     }
+    
     return (
         <div styel={{
             display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh',
